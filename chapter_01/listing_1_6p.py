dd5087736e47d7f68a0f02cffadb8cacb81fb875
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-12-08 16:34:36 +0800

    Add .ignore

---
+++ chapter_01/listing_1_6p.py
@@ -0,0 +1,34 @@
+import multiprocessing
+import time
+
+
+def print_fib(number: int) -> None:
+    def fib(n: int) -> int:
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
+
+    print(f'fib({number}) is {fib(number)}')
+
+
+def fibs_with_threads():
+    fortieth_thread = multiprocessing.Process(target=print_fib, args=(40,))
+    forty_first_thread = multiprocessing.Process(target=print_fib, args=(41,))
+
+    fortieth_thread.start()
+    forty_first_thread.start()
+
+    fortieth_thread.join()
+    forty_first_thread.join()
+
+
+start_threads = time.time()
+
+fibs_with_threads()
+
+end_threads = time.time()
+
+print(f'Threads took {end_threads - start_threads:.4f} seconds.')
